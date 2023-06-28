0c27ed29f005881dc862610cd23704db2e38bea4
Andrei Floroiu <floroiu.andrew@gmail.com>
2023-06-28 21:58:33 +0300

    Intermediary commit, work in progress.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ALEX",
@@ -15,11 +12,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body className="h-full">{children}</body>
     </html>
   );
 }
---
+++ components/layout/navbar/index.tsx
@@ -0,0 +1,3 @@
+export default async function Navbar() {
+  return <nav></nav>;
+}
