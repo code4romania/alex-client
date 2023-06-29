1273cf0d604325de1d855f4fe122863ad22429fa
Andrei Floroiu <floroiu.andrew@gmail.com>
2023-06-29 06:45:02 +0000

    Fixed 404 page. Fixed git ignore for devcontainers.

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,12 +14,12 @@ export default function Alex404() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              href="#"
+              href="/"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </Link>
-            <Link href="#" className="text-sm font-semibold text-gray-900">
+            <Link href="https://code4.ro" className="text-sm font-semibold text-gray-900">
               Contact support <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+export default class PagesDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en" className="h-full bg-white">
+        <Head>
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body className="h-full">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
