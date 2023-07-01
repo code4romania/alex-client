4f41f82778284d6eb4dac7f76bcfb07a0b7f6107
Andrei Floroiu <floroiu.andrew@gmail.com>
2023-07-01 23:26:07 +0300

    Customized page.

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,8 @@ export default function Home() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        <p className="text-base font-semibold leading-7 text-indigo-600">
-          Introducing
-        </p>
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          JavaScript for Beginners
+          Organization Profile
         </h1>
         <p className="mt-6 text-xl leading-8">
           Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
