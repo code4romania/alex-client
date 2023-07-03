e4c179593694dca1935108ac19a7b62678e6bffc
Andrei Floroiu <floroiu.andrew@gmail.com>
2023-07-03 19:18:37 +0000

    Changed demo.

--- components/layout/navheader/index.tsx
+++ components/layout/navheader/index.tsx
@@ -9,7 +9,6 @@ import {
 } from "@heroicons/react/20/solid";
 
 const userNavigation = [
-  { name: "Your profile", href: "#" },
   { name: "Sign out", href: "#" },
 ];
 
@@ -34,30 +33,12 @@ export default async function Navheader() {
       <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
 
       <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-        <form className="relative flex flex-1" action="#" method="GET">
-          <label htmlFor="search-field" className="sr-only">
-            Search
-          </label>
-          <MagnifyingGlassIcon
-            className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          <input
-            id="search-field"
+        <div className="relative flex flex-1">
+          <div
             className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-            placeholder="Search..."
-            type="search"
-            name="search"
           />
-        </form>
+        </div>
         <div className="flex items-center gap-x-4 lg:gap-x-6">
-          <button
-            type="button"
-            className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-          >
-            <span className="sr-only">View notifications</span>
-            <BellIcon className="h-6 w-6" aria-hidden="true" />
-          </button>
 
           {/* Separator */}
           <div
@@ -69,17 +50,15 @@ export default async function Navheader() {
           <Menu as="div" className="relative">
             <Menu.Button className="-m-1.5 flex items-center p-1.5">
               <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full bg-gray-50"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
+              <span className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-500">
+                <span className="text-sm font-medium leading-none text-white">II</span>
+              </span>
               <span className="hidden lg:flex lg:items-center">
                 <span
                   className="ml-4 text-sm font-semibold leading-6 text-gray-900"
                   aria-hidden="true"
                 >
-                  Tom Cook
+                  Ion Ionel
                 </span>
                 <ChevronDownIcon
                   className="ml-2 h-5 w-5 text-gray-400"
